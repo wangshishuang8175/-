f5a2ed45b51d453027f8ccd0dc345821fdd93b43
wangshishuang <835927863@qq.com>
2025-03-22 17:44:32 +0800

    f12闪烁

--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import './mock' // 引入mock服务
+
+// 添加 ResizeObserver 错误处理
+const debounce = (fn, delay) => {
+  let timeoutId
+  return (...args) => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn.apply(this, args), delay)
+  }
+}
+
+const handleError = debounce(() => {
+  const resizeObserverError = 'ResizeObserver loop completed with undelivered notifications.'
+  const error = console.error
+  console.error = (...args) => {
+    if (args[0] === resizeObserverError) {
+      return
+    }
+    error.apply(console, args)
+  }
+}, 0)
+
+handleError()
 
 const app = createApp(App)
 const pinia = createPinia()
